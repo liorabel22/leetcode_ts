8b1dabdeb5b18c1c055040c08c7c3b1404ebd2bb
liorabel22 <60816560+liorabel22@users.noreply.github.com>
2026-01-23 15:28:59 +0200

    fix aliases

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,10 @@
-import path from 'node:path'
-
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
+  plugins: [tsconfigPaths()],
   test: {
     globals: true,
-    environment: 'node',
-    include: ['tests/**/*.test.ts'],
-  },
-  resolve: {
-    alias: {
-      '@arrays': path.resolve(__dirname, 'src/arrays'),
-      '@linked-list': path.resolve(__dirname, 'src/linked-list'),
-    }
+    environment: 'node'
   }
 })
